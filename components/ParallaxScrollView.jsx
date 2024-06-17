5a742090f61e32b3a8683f6fb377253ceeaa718c
Kota Amemiya <kotaamemiya@gmail.com>
2024-06-17 16:05:48 +0100

    Convert TypeScript to JavaScript

--- components/ParallaxScrollView.jsx
+++ components/ParallaxScrollView.jsx
@@ -1,4 +1,4 @@
-import type { PropsWithChildren, ReactElement } from 'react';
+import React from 'react';
 import { StyleSheet, useColorScheme } from 'react-native';
 import Animated, {
   interpolate,
@@ -11,18 +11,13 @@ import { ThemedView } from '@/components/ThemedView';
 
 const HEADER_HEIGHT = 250;
 
-type Props = PropsWithChildren<{
-  headerImage: ReactElement;
-  headerBackgroundColor: { dark: string; light: string };
-}>;
-
 export default function ParallaxScrollView({
   children,
   headerImage,
   headerBackgroundColor,
-}: Props) {
+}) {
   const colorScheme = useColorScheme() ?? 'light';
-  const scrollRef = useAnimatedRef<Animated.ScrollView>();
+  const scrollRef = useAnimatedRef();
   const scrollOffset = useScrollViewOffset(scrollRef);
 
   const headerAnimatedStyle = useAnimatedStyle(() => {
