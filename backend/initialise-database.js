16028428df163085f54af197a432cfcc2eb3f847
dhgoktan@icloud.com <dhgoktan@icloud.com>
2024-08-08 18:16:16 +0100

    adding to database

--- backend/initialise-database.js
+++ backend/initialise-database.js
@@ -9,10 +9,17 @@ const createTables = async () => {
                 username VARCHAR(50) UNIQUE NOT NULL,
                 email VARCHAR(100) UNIQUE NOT NULL,
                 password_hash VARCHAR(255) NOT NULL,
+                password_salt VARCHAR(255) NOT NULL,
+                token_expiration TIMESTAMP,
+                links VARCHAR(100),
                 bio TEXT,
                 profile_picture VARCHAR(255),
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                   CONSTRAINT check_password CHECK (
+                        password_hash ~* '^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+                    )
+
             );
 
             CREATE TABLE IF NOT EXISTS artworks (
@@ -27,10 +34,10 @@ const createTables = async () => {
             );
             
             CREATE TABLE IF NOT EXISTS likes (
-                id SERIAL PRIMARY KEY,
                 user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
                 artwork_id INTEGER REFERENCES artworks(id) ON DELETE CASCADE,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                  PRIMARY KEY (user_id, artwork_id)
             );
             
             CREATE TABLE IF NOT EXISTS comments (
@@ -56,6 +63,17 @@ const createTables = async () => {
                 status VARCHAR(50) NOT NULL,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+
+            CREATE TABLE IF NOT EXISTS tournaments (
+                id SERIAL PRIMARY KEY,
+                host_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+                title VARCHAR(100) NOT NULL,
+                description TEXT,
+                start_time TIMESTAMP NOT NULL,
+                end_time TIMESTAMP NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+           
             );
         `);
         console.log('Tables created successfully');
